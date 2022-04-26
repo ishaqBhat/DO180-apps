770382995eee6bef4c92ae4bfd117278912f01c5
Student User <student@workstation.lab.example.com>
2022-04-26 05:53:19 -0400

    Web Page changed and container rebuild and re-Deployed

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, MR MOHAMMAD ISHAQ BHAT You have Changed home page and Rebuild and re deployed the container again...Thank you ! php version is " . PHP_VERSION . "\n";
 ?>
